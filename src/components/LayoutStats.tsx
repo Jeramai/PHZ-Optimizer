29b250f54e401b1c8bdc89d55118beb65bdc3bd6
Jeramai <j.faber@provrex.nl>
2025-04-04 18:16:41 +0700

    Refactor LayoutStats to return separate stats and hex results for improved display

--- src/components/LayoutStats.tsx
+++ src/components/LayoutStats.tsx
@@ -15,10 +15,11 @@ export default function LayoutStats({ data }: Readonly<{ data: Item[] }>) {
     return (color: string): [BuffType, number] | undefined => bonusMap[color];
   }, []);
 
-  const statElements = useMemo(() => {
-    if (!data?.length) return [];
+  const { statsResult, hexResult } = useMemo(() => {
+    if (!data?.length) return { statsResult: [], hexResult: [] };
 
     const stats = new Map<BuffType | Attribute, number>();
+    const hex = new Map<BuffType | Attribute, number>();
 
     // Process item buffs
     data.forEach((element) => {
@@ -43,12 +44,13 @@ export default function LayoutStats({ data }: Readonly<{ data: Item[] }>) {
       if (bonus) {
         const [type, amount] = bonus;
         stats.set(type, (stats.get(type) ?? 0) + amount);
+        hex.set(type, (hex.get(type) ?? 0) + amount);
       }
     });
 
     const priorityOrder = ['Basic', 'Boss', 'Skill', 'All', 'Crit D.'];
 
-    return Array.from(stats.entries())
+    const statsResult = Array.from(stats.entries())
       .sort(([nameA, amountA], [nameB, amountB]) => {
         const indexA = priorityOrder.indexOf(nameA);
         const indexB = priorityOrder.indexOf(nameB);
@@ -64,7 +66,48 @@ export default function LayoutStats({ data }: Readonly<{ data: Item[] }>) {
           <strong>{Number(amount.toFixed(2))}%</strong>
         </div>
       ));
+    const hexResult = Array.from(hex.entries())
+      .sort(([nameA, amountA], [nameB, amountB]) => {
+        const indexA = priorityOrder.indexOf(nameA);
+        const indexB = priorityOrder.indexOf(nameB);
+
+        if (indexA !== -1 && indexB !== -1) return indexA - indexB;
+        if (indexA !== -1) return -1;
+        if (indexB !== -1) return 1;
+        return amountB - amountA;
+      })
+      .map(([name, amount]) => (
+        <div
+          key={name}
+          className={`${
+            name === 'Boss'
+              ? 'text-red-600 dark:text-red-400 '
+              : name === 'Skill'
+              ? 'text-orange-600 dark:text-orange-300 '
+              : name === 'All'
+              ? 'text-blue-600 dark:text-blue-400 '
+              : name === 'Crit D.'
+              ? 'text-purple-600 dark:text-purple-400'
+              : ''
+          }`}
+        >
+          <span>{name}: </span>
+          <strong>{Number(amount.toFixed(2))}%</strong>
+        </div>
+      ));
+
+    return { statsResult, hexResult };
   }, [data, getColorBonus]);
 
-  return statElements.length ? <div className='grid grid-cols-2 gap-x-5'>{statElements}</div> : null;
+  return (
+    <>
+      {hexResult.length ? <div className='w-full flex justify-evenly gap-x-5'>{hexResult}</div> : null}
+      {statsResult.length ? (
+        <div className='w-full flex flex-col items-center'>
+          <strong>Total:</strong>
+          <div className='grid grid-cols-2 gap-x-5'>{statsResult}</div>
+        </div>
+      ) : null}
+    </>
+  );
 }
